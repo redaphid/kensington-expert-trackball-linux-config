226d8f1d355426e5e0a037d7946b0d8d439cc22a
redaphid <iam@aaronherres.com>
2021-11-11 23:29:53 -0700

    don't do fancy scrolling so I can hold the mouse button and click

--- Kensington_Expert_Setup.sh
+++ Kensington_Expert_Setup.sh
@@ -23,14 +23,14 @@ if [[ ! -z "$check" ]]; then
 	# enable better scrolling 
 	xinput set-button-map $mouse_id 3 8 1 4 5 6 7 2 9
 #	xinput set-button-map $mouse_id 1 8 2 4 5 6 7 3 9
-	xinput set-prop $mouse_id "libinput Natural Scrolling Enabled" 1
+#	xinput set-prop $mouse_id "libinput Natural Scrolling Enabled" 1
 	# disable acceliration for the ball
-	xinput set-prop $mouse_id "libinput Accel Profile Enabled" 0, 1
+#	xinput set-prop $mouse_id "libinput Accel Profile Enabled" 0, 1
 
 	# allow scrolling by holding middle mouse button and using the ball to scroll ( really smooth and fast ). 
-	xinput set-prop $mouse_id "libinput Scroll Method Enabled" 0, 0, 1
+#	xinput set-prop $mouse_id "libinput Scroll Method Enabled" 0, 0, 1
 	# allow the remmaped middle mouse to be used for middle mouse scroll
-	xinput set-prop $mouse_id "libinput Button Scrolling Button" 3
+#	xinput set-prop $mouse_id "libinput Button Scrolling Button" 3
 fi
 
 # read more here https://askubuntu.com/questions/492744/how-do-i-automatically-remap-buttons-on-my-mouse-at-startup
